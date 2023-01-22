556760931d12023059a0984a03823f07c45a1c69
smdimran2000 <shaikmdimran31@gmail.com>
2023-01-22 12:24:46 +0530

    commit

---
+++ website-2.0/src/test/java/com/mywebsite20/website20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mywebsite20.website20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
